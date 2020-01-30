9c0cb87e151929619ffe03ad924e881f658077f2
Peter Schneider <Peter.Schneider@plan-b-gmbh.com>
2020-01-30 14:38:30 +0100

    -change in the upload excel the ü to ue

--- ButlerBot/Dialogs/InterruptDialog.cs
+++ ButlerBot/Dialogs/InterruptDialog.cs
@@ -152,7 +152,7 @@
                     await innerDc.Context.SendActivityAsync(MessageFactory.Text($"Einen Moment ich suche schnell alles zusammen!"), cancellationToken);
                     // string[] name = innerDc.Context.Activity.From.Name.Split(' ');
                     getExcel.Run();// name[0] + name[1][0]
-                    var temp = BotMethods.GetDocument("excel", "Monatsübersicht_" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Year + ".xlsx");
+                    var temp = BotMethods.GetDocument("excel", "Monatsuebersicht_" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Year + ".xlsx");
                     //session.send(temp);
                     
                     //await innerDc.Context.SendActivityAsync(MessageFactory.Text(message), cancellationToken);
