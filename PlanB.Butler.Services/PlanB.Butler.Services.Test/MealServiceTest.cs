be4901e7f8189e4bcb03ac9b4a7c103d161960ce
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2020-02-21 07:46:22 +0100

    #66 Get Meals by date

--- PlanB.Butler.Services/PlanB.Butler.Services.Test/MealServiceTest.cs
+++ PlanB.Butler.Services/PlanB.Butler.Services.Test/MealServiceTest.cs
@@ -1,6 +1,8 @@
 // Copyright (c) PlanB. GmbH. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PlanB.Butler.Services.Test
@@ -79,6 +81,114 @@ namespace PlanB.Butler.Services.Test
         {
             var result = MealService.CreateBlobPrefix("2020-02-25", "2020-03-0");
             Assert.AreEqual(string.Empty, result);
+        }
+
+        /// <summary>
+        /// Determines whether date is in range ok.
+        /// </summary>
+        [TestMethod]
+        public void IsDateInRangeOK()
+        {
+            var start = "2020-02-01";
+            var end = "2020-02-12";
+            var toCheck = "2020-02-10";
+
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            var toCheckDate = DateTime.Parse(toCheck);
+
+            var result = MealService.IsDateInRange(startDate, endDate, toCheckDate);
+            Assert.AreEqual(true, result);
+        }
+
+        /// <summary>
+        /// Determines whether date is too early.
+        /// </summary>
+        [TestMethod]
+        public void IsDateInRangeCheckDateEarlierOK()
+        {
+            var start = "2020-02-01";
+            var end = "2020-02-12";
+            var toCheck = "2020-01-10";
+
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            var toCheckDate = DateTime.Parse(toCheck);
+
+            var result = MealService.IsDateInRange(startDate, endDate, toCheckDate);
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// Determines whether date is too late.
+        /// </summary>
+        [TestMethod]
+        public void IsDateInRangeCheckDateLaterOK()
+        {
+            var start = "2020-02-01";
+            var end = "2020-02-12";
+            var toCheck = "2021-01-10";
+
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            var toCheckDate = DateTime.Parse(toCheck);
+
+            var result = MealService.IsDateInRange(startDate, endDate, toCheckDate);
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// Determines whether all dates are equal.
+        /// </summary>
+        [TestMethod]
+        public void IsDateInRangeCheckDateAllEqual()
+        {
+            var start = "2020-02-01";
+            var end = "2020-02-01";
+            var toCheck = "2020-02-01";
+
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            var toCheckDate = DateTime.Parse(toCheck);
+
+            var result = MealService.IsDateInRange(startDate, endDate, toCheckDate);
+            Assert.AreEqual(true, result);
+        }
+
+        /// <summary>
+        /// Determines whether Startdate is equal to CheckDate.
+        /// </summary>
+        [TestMethod]
+        public void IsDateInRangeCheckStartEqualCheck()
+        {
+            var start = "2020-02-01";
+            var end = "2020-12-01";
+            var toCheck = "2020-02-01";
+
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            var toCheckDate = DateTime.Parse(toCheck);
+
+            var result = MealService.IsDateInRange(startDate, endDate, toCheckDate);
+            Assert.AreEqual(true, result);
+        }
+
+        /// <summary>
+        /// Determines whether Enddate is equal to CheckDate.
+        /// </summary>
+        [TestMethod]
+        public void IsDateInRangeCheckEndEqualCheck()
+        {
+            var start = "2020-01-01";
+            var end = "2020-02-01";
+            var toCheck = "2020-02-01";
+
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            var toCheckDate = DateTime.Parse(toCheck);
+
+            var result = MealService.IsDateInRange(startDate, endDate, toCheckDate);
+            Assert.AreEqual(true, result);
         }
     }
 }
