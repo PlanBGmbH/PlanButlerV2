7b04146432f92a34da00a849a10f6fc8dbc3acab
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2020-02-23 08:27:15 +0100

    #69 Add Restaurants

---
+++ PlanB.Butler.Services/PlanB.Butler.Services/Models/RestaurantModel.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) PlanB. GmbH. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanB.Butler.Services.Models
+{
+    /// <summary>
+    /// RestaurantModel.
+    /// </summary>
+    public class RestaurantModel
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street.
+        /// </summary>
+        /// <value>
+        /// The street.
+        /// </value>
+        public string Street { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postal code.
+        /// </summary>
+        /// <value>
+        /// The postal code.
+        /// </value>
+        public string PostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email adress.
+        /// </summary>
+        /// <value>
+        /// The email adress.
+        /// </value>
+        public string EmailAdress { get; set; }
+    }
+}
