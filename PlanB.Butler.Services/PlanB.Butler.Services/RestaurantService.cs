8791dfacb53b3eb4c5c0ccddd550668518b28f6a
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2020-02-24 07:15:55 +0100

    #66 Create Restaurant

--- PlanB.Butler.Services/PlanB.Butler.Services/RestaurantService.cs
+++ PlanB.Butler.Services/PlanB.Butler.Services/RestaurantService.cs
@@ -3,11 +3,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -15,6 +17,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
 using PlanB.Butler.Services.Extensions;
 using PlanB.Butler.Services.Models;
 
@@ -26,15 +29,13 @@ namespace PlanB.Butler.Services
     public static class RestaurantService
     {
         /// <summary>
-        /// Reads the meals.
+        /// Gets the restaurants.
         /// </summary>
         /// <param name="req">The req.</param>
         /// <param name="cloudBlobContainer">The cloud BLOB container.</param>
         /// <param name="log">The log.</param>
         /// <param name="context">The context.</param>
-        /// <returns>
-        /// All meals.
-        /// </returns>
+        /// <returns>All Restaurants.</returns>
         [ProducesResponseType(typeof(List<RestaurantModel>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
         [FunctionName("GetRestaurants")]
@@ -84,6 +85,81 @@ namespace PlanB.Butler.Services
                     Message = e.Message,
                 };
                 actionResult = new BadRequestObjectResult(restaurant);
+            }
+            finally
+            {
+                log.LogTrace(eventId, $"'{methodName}' - busobjkey finished");
+                log.LogInformation(correlationId, $"'{methodName}' - finished", trace);
+            }
+
+            return actionResult;
+        }
+
+        /// <summary>
+        /// Creates the restaurant.
+        /// </summary>
+        /// <param name="req">The req.</param>
+        /// <param name="cloudBlobContainer">The cloud BLOB container.</param>
+        /// <param name="log">The log.</param>
+        /// <param name="context">The context.</param>
+        /// <returns>IActionResult.</returns>
+        [FunctionName("CreateRestaurant")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorModel), StatusCodes.Status400BadRequest)]
+        public static async Task<IActionResult> CreateRestaurant(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "restaurants")]
+            [RequestBodyType(typeof(RestaurantModel), "Restaurant request")]HttpRequest req,
+            [Blob("restaurants", FileAccess.ReadWrite, Connection = "StorageSend")] CloudBlobContainer cloudBlobContainer,
+            ILogger log,
+            ExecutionContext context)
+        {
+            Guid correlationId = Util.ReadCorrelationId(req.Headers);
+            var methodName = MethodBase.GetCurrentMethod().Name;
+            var trace = new Dictionary<string, string>();
+            EventId eventId = new EventId(correlationId.GetHashCode(), Constants.ButlerCorrelationTraceName);
+
+            IActionResult actionResult = null;
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                trace.Add("requestBody", requestBody);
+
+                RestaurantModel restaurantModel = JsonConvert.DeserializeObject<RestaurantModel>(requestBody);
+
+                var filename = $"{restaurantModel.Name}.json";
+                trace.Add($"filename", filename);
+
+                req.HttpContext.Response.Headers.Add(Constants.ButlerCorrelationTraceHeader, correlationId.ToString());
+
+                CloudBlockBlob blob = cloudBlobContainer.GetBlockBlobReference($"{filename}");
+                if (blob != null)
+                {
+                    blob.Properties.ContentType = "application/json";
+                    blob.Metadata.Add(Constants.ButlerCorrelationTraceName, correlationId.ToString().Replace("-", string.Empty));
+                    var restaurant = JsonConvert.SerializeObject(restaurantModel);
+                    trace.Add("restaurant", restaurant);
+
+                    Task task = blob.UploadTextAsync(requestBody);
+                }
+
+                log.LogInformation(correlationId, $"'{methodName}' - success", trace);
+                actionResult = new OkResult();
+            }
+            catch (Exception e)
+            {
+                trace.Add(string.Format("{0} - {1}", MethodBase.GetCurrentMethod().Name, "rejected"), e.Message);
+                trace.Add(string.Format("{0} - {1} - StackTrace", MethodBase.GetCurrentMethod().Name, "rejected"), e.StackTrace);
+                log.LogInformation(correlationId, $"'{methodName}' - rejected", trace);
+                log.LogError(correlationId, $"'{methodName}' - rejected", trace);
+                ErrorModel errorModel = new ErrorModel()
+                {
+                    CorrelationId = correlationId,
+                    Details = e.StackTrace,
+                    Message = e.Message,
+                };
+
+                actionResult = new BadRequestObjectResult(errorModel);
+                throw;
             }
             finally
             {
